10e65354e36e4cfe9a70d208120128f509aaf696
Pug97 <pug.97820@gmail.com>
2026-03-07 11:58:04 +0000

    script.js

--- script.js
+++ script.js
@@ -5,29 +5,35 @@ const itemsContainer = document.getElementById("items")
 
 const gifts = [
 
-"Common 🎁",
-"Common 🎁",
-"Common 🎁",
-"Rare 💎",
-"Rare 💎",
-"Epic 🔥",
-"Legendary 👑"
+{name:"Small Gift", class:"common"},
+{name:"Angel Feather", class:"common"},
+{name:"Golden Wing", class:"rare"},
+{name:"Heaven Box", class:"rare"},
+{name:"Divine Halo", class:"epic"},
+{name:"Sacred Relic", class:"epic"},
+{name:"Angel Crown", class:"legendary"}
 
 ]
+
+function randomGift(){
+
+return gifts[Math.floor(Math.random()*gifts.length)]
+
+}
 
 function generateItems(){
 
 itemsContainer.innerHTML=""
 
-for(let i=0;i<50;i++){
+for(let i=0;i<60;i++){
 
-const gift = gifts[Math.floor(Math.random()*gifts.length)]
+const gift = randomGift()
 
 const div = document.createElement("div")
 
-div.className="item"
+div.className="item "+gift.class
 
-div.innerText=gift
+div.innerText=gift.name
 
 itemsContainer.appendChild(div)
 
@@ -37,21 +43,21 @@ itemsContainer.appendChild(div)
 
 generateItems()
 
-document.getElementById("openCase").onclick = function(){
+document.getElementById("openCase").onclick=function(){
 
 generateItems()
 
-const winIndex = Math.floor(Math.random()*40)+5
+const winIndex = Math.floor(Math.random()*40)+10
 
-const offset = winIndex * 120
+const offset = winIndex * 140
 
-itemsContainer.style.transform = `translateX(-${offset}px)`
+itemsContainer.style.transform=`translateX(-${offset}px)`
 
 const winItem = itemsContainer.children[winIndex].innerText
 
 setTimeout(()=>{
 
-document.getElementById("result").innerText = "You won: " + winItem
+document.getElementById("result").innerText="You won: "+winItem
 
 },4000)
 
