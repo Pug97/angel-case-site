72798a3daa1e8d06d1e1cfaf74fdfa8c773f087f
Pug97 <pug.97820@gmail.com>
2026-03-07 12:40:22 +0000

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-let spinning = false
-
 const tg = window.Telegram.WebApp
 tg.expand()
 
@@ -7,6 +5,7 @@ const itemsContainer = document.getElementById("items")
 
 let idleOffset = 0
 let idleRunning = true
+let spinning = false
 
 const gifts = [
 
@@ -35,6 +34,7 @@ const gift = randomGift()
 const div = document.createElement("div")
 
 div.className="item "+gift.class
+
 div.innerText = gift.name
 
 itemsContainer.appendChild(div)
@@ -45,7 +45,8 @@ itemsContainer.appendChild(div)
 
 generateItems()
 
-// idle движение
+
+// idle анимация рулетки
 function idleAnimation(){
 
 if(!idleRunning) return
@@ -62,8 +63,12 @@ idleAnimation()
 
 
 
-// кнопка открытия
+// кнопка открытия кейса
 document.getElementById("openCase").onclick=function(){
+
+if(spinning) return
+
+spinning = true
 
 idleRunning = false
 
@@ -76,7 +81,7 @@ const randomOffset = Math.random()*2000 + 1500
 itemsContainer.style.transform=`translateX(-${randomOffset}px)`
 
 
-// после остановки определяем предмет под стрелкой
+// после остановки рулетки
 setTimeout(()=>{
 
 const markerX = window.innerWidth / 2
@@ -103,6 +108,8 @@ showWinPopup(winItem.innerText)
 
 }
 
+spinning = false
+
 idleRunning = true
 idleAnimation()
 
@@ -110,6 +117,9 @@ idleAnimation()
 
 }
 
+
+
+// popup окно выигрыша
 function showWinPopup(prize){
 
 document.getElementById("popupItem").innerText = prize
@@ -118,6 +128,9 @@ document.getElementById("winPopup").style.display = "flex"
 
 }
 
+
+
+// кнопка забрать
 document.getElementById("claimBtn").onclick=function(){
 
 document.getElementById("winPopup").style.display = "none"
