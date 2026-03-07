fc9cae8fd3ce92e5adf9c2723ab5521092fe52c0
Pug97 <pug.97820@gmail.com>
2026-03-07 12:58:04 +0000

    Update script.js

--- script.js
+++ script.js
@@ -32,7 +32,6 @@ const gift=randomGift()
 const div=document.createElement("div")
 
 div.className="item "+gift.class
-
 div.innerText=gift.name
 
 itemsContainer.appendChild(div)
@@ -52,7 +51,6 @@ if(!idleRunning) return
 currentOffset+=0.25
 
 itemsContainer.style.transition="none"
-
 itemsContainer.style.transform=`translateX(-${currentOffset}px)`
 
 requestAnimationFrame(idleAnimation)
@@ -68,17 +66,20 @@ document.getElementById("openCase").onclick=function(){
 if(spinning) return
 
 spinning=true
-
 idleRunning=false
 
-itemsContainer.style.transition="transform 4s cubic-bezier(.17,.67,.24,1)"
+/* 5 секунд прокрутки */
+
+itemsContainer.style.transition="transform 5s cubic-bezier(.17,.67,.24,1)"
 
 generateItems()
 
-currentOffset+=Math.random()*1500+1500
+currentOffset+=Math.random()*2000+2000
 
 itemsContainer.style.transform=`translateX(-${currentOffset}px)`
 
+
+/* ожидание окончания прокрутки */
 
 setTimeout(()=>{
 
@@ -107,11 +108,10 @@ showWinPopup(winItem.innerText)
 }
 
 spinning=false
-
 idleRunning=true
 idleAnimation()
 
-},4000)
+},5000)
 
 }
 
