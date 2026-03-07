d898b996957f2c7d152b6f277df060ababa634f1
Pug97 <pug.97820@gmail.com>
2026-03-07 12:47:44 +0000

    Update script.js

--- script.js
+++ script.js
@@ -33,7 +33,8 @@ const gift = randomGift()
 
 const div = document.createElement("div")
 
-div.className = "item " + gift.class
+div.className="item "+gift.class
+
 div.innerText = gift.name
 
 itemsContainer.appendChild(div)
@@ -45,15 +46,16 @@ itemsContainer.appendChild(div)
 generateItems()
 
 
-// idle движение рулетки
+
 function idleAnimation(){
 
 if(!idleRunning) return
 
 idleOffset += 0.25
 
-itemsContainer.style.transition = "none"
-itemsContainer.style.transform = `translateX(-${idleOffset}px)`
+itemsContainer.style.transition="none"
+
+itemsContainer.style.transform=`translateX(-${idleOffset}px)`
 
 requestAnimationFrame(idleAnimation)
 
@@ -62,40 +64,39 @@ requestAnimationFrame(idleAnimation)
 idleAnimation()
 
 
-// кнопка открытия кейса
-document.getElementById("openCase").onclick = function(){
+
+document.getElementById("openCase").onclick=function(){
 
 if(spinning) return
 
-spinning = true
+spinning=true
 
-idleRunning = false
+idleRunning=false
 
-itemsContainer.style.transition = "transform 4s cubic-bezier(.17,.67,.24,1)"
+itemsContainer.style.transition="transform 4s cubic-bezier(.17,.67,.24,1)"
 
 generateItems()
 
 const randomOffset = Math.random()*2000 + 1500
 
-itemsContainer.style.transform = `translateX(-${randomOffset}px)`
+itemsContainer.style.transform=`translateX(-${randomOffset}px)`
 
 
-// после остановки рулетки
 setTimeout(()=>{
 
-const markerX = window.innerWidth / 2
+const markerX = window.innerWidth/2
 
-const items = document.querySelectorAll(".item")
+const items=document.querySelectorAll(".item")
 
-let winItem = null
+let winItem=null
 
 items.forEach(item=>{
 
-const rect = item.getBoundingClientRect()
+const rect=item.getBoundingClientRect()
 
 if(rect.left < markerX && rect.right > markerX){
 
-winItem = item
+winItem=item
 
 }
 
@@ -107,9 +108,9 @@ showWinPopup(winItem.innerText)
 
 }
 
-spinning = false
+spinning=false
 
-idleRunning = true
+idleRunning=true
 idleAnimation()
 
 },4000)
@@ -118,20 +119,18 @@ idleAnimation()
 
 
 
-// popup окно
 function showWinPopup(prize){
 
-document.getElementById("popupItem").innerText = prize
+document.getElementById("popupItem").innerText=prize
 
-document.getElementById("winPopup").style.display = "flex"
+document.getElementById("winPopup").style.display="flex"
 
 }
 
 
 
-// кнопка забрать
-document.getElementById("claimBtn").onclick = function(){
+document.getElementById("claimBtn").onclick=function(){
 
-document.getElementById("winPopup").style.display = "none"
+document.getElementById("winPopup").style.display="none"
 
 }
